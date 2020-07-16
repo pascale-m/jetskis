953fa6d776a69d654a424b08f4024ad8aa5397b4
pascale-m <pascale@djmgy.com>
2020-07-15 23:17:36 -0700

    init commit

---
+++ downloadpages.js
@@ -0,0 +1,5 @@
+function downloadedPage(event) {
+    //change to file transfer graphic
+    document.getElementById("downloadbox").innerHTML = '<div class="flex-item" id="downloading"><img id="filetransfer" src="media/filetransfer3.gif"></div>';
+    //after x seconds, change to download complete graphic
+}
