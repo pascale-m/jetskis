f5376f4246ec26e42150232eff6d4b857adfcbe6
pascale-m <pascale@djmgy.com>
2020-07-16 00:08:03 -0700

    page advance, timer and gif selection complete

--- downloadpages.js
+++ downloadpages.js
@@ -1,5 +1,10 @@
+function complete() {
+    document.getElementById("content").innerHTML = '<img id="downloadcomplete" src="media/thankyou.gif">';
+}
+
 function downloadedPage(event) {
     //change to file transfer graphic
-    document.getElementById("downloadbox").innerHTML = '<div class="flex-item" id="downloading"><img id="filetransfer" src="media/filetransfer3.gif"></div>';
+    document.getElementById("content").innerHTML = '<img id="filetransfer" src="media/filetransfer3.gif">';
     //after x seconds, change to download complete graphic
+    setTimeout(complete, 3000);
 }
